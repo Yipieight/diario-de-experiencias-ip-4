886c0a765d95c5744c02000d40d20a87872de626
Yipieight <57154305+Yipieight@users.noreply.github.com>
2023-02-18 11:54:53 -0600

    ejercicio no terminando

---
+++ Semana 05/Laboratorio_01_04/Laboratorio_01_04/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejercicio 1: Operaciones aritmeticas. ");
+Console.Write("Ingrese un numero: ");
+long n1 = Convert.ToInt64(Console.ReadLine());
+
+Console.Write("Ingrese un numero: ");
+long n2 = Convert.ToInt64(Console.ReadLine());
+
+Console.ReadKey();
