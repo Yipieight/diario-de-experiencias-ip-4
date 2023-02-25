9f60994b2aa2505ce606452f7dbb6c944beaf613
Yipieight <57154305+Yipieight@users.noreply.github.com>
2023-02-25 12:01:36 -0600

    ar

--- Semana 05/Laboratorio_01_04/Laboratorio_01_04/Program.cs
+++ Semana 05/Laboratorio_01_04/Laboratorio_01_04/Program.cs
@@ -1,9 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ejercicio 1: Operaciones aritmeticas. ");
 Console.Write("Ingrese un numero: ");
-long n1 = Convert.ToInt64(Console.ReadLine());
+int n1 = Convert.ToInt32(Console.ReadLine()); //5
 
 Console.Write("Ingrese un numero: ");
-long n2 = Convert.ToInt64(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine()); //3
 
-Console.ReadKey();
+
+int suma = n1 + n2;
+Console.WriteLine(n1 + " + " + n2 + " = " + suma);
+
+int resta = n1 - n2;
+Console.WriteLine(n1 + " - " + n2 + " = " + resta);
+
+int multi = n1 * n2;
+Console.WriteLine(n1 + " * " + n2 + " = " + multi);
+
+//double n1d = n1;
+//double n2d = n2;
+
+double division = (double)n1 / n2;
+Console.WriteLine(n1 + " / " + n2 + " = " + division);
+
+int div1 = n1 / n2;
+Console.WriteLine(n1 + " / " + n2 + " = " + div1);
+
+int mod1 = n1 % n2;
+Console.WriteLine(n1 + " / " + n2 + " = " + mod1);
+
+//EJERCICIO 2
+
+
